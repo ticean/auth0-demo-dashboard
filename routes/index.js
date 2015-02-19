4a7dfeb1c7dd32ac4b3db64b4fda0e2f004eae6e
Ticean Bennett <ticean@gmail.com>
2015-02-18 21:13:03 -0800

    Streams with role authorization

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,65 @@
+var _ = require('underscore');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     if (!req.user) {
       throw new Error('user null');
     }
-    var apps = [];
-    if (req.user._json && req.user._json.apps) {
-      apps = req.user._json.apps
-    }
-    res.render('index', {
-      apps: JSON.stringify(apps),
-      displayName: req.user.displayName,
-      picture: req.user.picture || 'https://graph.facebook.com/3/picture',
-      menuLinks: {
-        sites:  process.env.SCALEWORKS_SITES_URL,
-        mail:   process.env.SCALEWORKS_MAIL_URL,
-        social: process.env.SCALEWORKS_SOCIAL_URL,
-        store:  process.env.SCALEWORKS_STORE_URL
-      }
-    });
+    next();
   } else {
     res.redirect('/login');
   }
+}
+
+function ensureAuthorizedByRole(role) {
+  return function(req, res, next) {
+    if (userHasRole(req.user, role)) {
+      next();
+    } else {
+      throw new Error('You are not authorized to perform this action.');
+    }
+  };
+}
+
+function userHasRole(user, role) {
+  roles = user._json.roles;
+  return _.contains(roles, role);
+}
+
+function commonLocals(req) {
+  return {
+    userData: JSON.stringify(req.user),
+    isUserAdmin: userHasRole(req.user, 'admin'),
+    displayName: req.user.displayName,
+    picture: req.user.picture || 'https://graph.facebook.com/3/picture',
+    menuLinks: {
+      sites:  process.env.SCALEWORKS_SITES_URL,
+      mail:   process.env.SCALEWORKS_MAIL_URL,
+      social: process.env.SCALEWORKS_SOCIAL_URL,
+      store:  process.env.SCALEWORKS_STORE_URL
+    }
+  };
+}
+
+router.get('/', ensureAuthenticated, function(req, res, next) {
+  res.render('index', commonLocals(req));
 });
 
-router.get('/account', function(req, res, next) {
-  if (req.isAuthenticated()) {
-    if (!req.user) {
-      throw new Error('user null');
-    }
-    res.render('account', {
-      userData: JSON.stringify(req.user),
-      displayName: req.user.displayName,
-      picture: req.user.picture || 'https://graph.facebook.com/3/picture',
-      menuLinks: {
-        sites:  process.env.SCALEWORKS_SITES_URL,
-        mail:   process.env.SCALEWORKS_MAIL_URL,
-        social: process.env.SCALEWORKS_SOCIAL_URL,
-        store:  process.env.SCALEWORKS_STORE_URL
-      }
-    });
-  } else {
-    res.redirect('/login');
-  }
+router.get('/account', ensureAuthenticated, function(req, res, next) {
+  res.render('account', commonLocals(req));
 });
+
+router.get('/streams', ensureAuthenticated, ensureAuthorizedByRole('admin'),
+  function(req, res, next) {
+    res.render('streams', commonLocals(req));
+  }
+);
 
 router.get('/login', function(req, res, next) {
-  res.locals.clientId = process.env.AUTH0_CLIENT_ID,
-  res.locals.auth0Domain = process.env.AUTH0_DOMAIN,
+  res.locals.clientId = process.env.AUTH0_CLIENT_ID;
+  res.locals.auth0Domain = process.env.AUTH0_DOMAIN;
   res.render('login', {layout: false});
 });
 
