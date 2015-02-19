2c0b0a5276046dd63e862f164b4569a7a108af98
Ticean Bennett <ticean@gmail.com>
2015-02-18 17:21:35 -0800

    Layouts and account page

--- routes/index.js
+++ routes/index.js
@@ -27,14 +27,31 @@ router.get('/', function(req, res, next) {
   }
 });
 
-router.get('/user', function(req, res, next) {
-  res.json(req.user);
+router.get('/account', function(req, res, next) {
+  if (req.isAuthenticated()) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.render('account', {
+      userData: JSON.stringify(req.user),
+      displayName: req.user.displayName,
+      picture: req.user.picture || 'https://graph.facebook.com/3/picture',
+      menuLinks: {
+        sites:  process.env.SCALEWORKS_SITES_URL,
+        mail:   process.env.SCALEWORKS_MAIL_URL,
+        social: process.env.SCALEWORKS_SOCIAL_URL,
+        store:  process.env.SCALEWORKS_STORE_URL
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 router.get('/login', function(req, res, next) {
   res.locals.clientId = process.env.AUTH0_CLIENT_ID,
   res.locals.auth0Domain = process.env.AUTH0_DOMAIN,
-  res.render('login');
+  res.render('login', {layout: false});
 });
 
 router.get('/logout', function(req, res, next) {
